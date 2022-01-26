c35aa6184ca9ceca173a4700c526a6786cccfe90
Momchil Marinov <mom4ilakis@gmail.com>
2022-01-26 18:49:28 +0200

    Add finish condition check Rename variable

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class Board extends React.Component {
         this.answer = 'weary';
         this.state = {
             attempts: [...Array(6).keys()].map(() => ''),
-            currentWord: 0
+            currentAttempt: 0,
+            gameFinished: false
         };
     }
 
@@ -23,33 +24,41 @@ class Board extends React.Component {
         document.removeEventListener('keyup', this.handleKeyPress);
     };
 
+    onFinish = () => {
+        document.removeEventListener('keyup', this.handleKeyPress);
+    };
+
     handleKeyPress = (event) => {
         const { key } = event;
-        const currentWordLen = this.state.attempts[this.state.currentWord].length;
+        const currentWordLen = this.state.attempts[this.state.currentAttempt].length;
 
         if ( key.length === 1 && key.match(/[a-z]/i) && currentWordLen < 5) {
             this.setState((state) => {
-                const {currentWord, attempts} = state;
-                const newWord = attempts[currentWord] + key;
+                const {currentAttempt, attempts} = state;
+                const newWord = attempts[currentAttempt] + key;
                 const newAttempts = attempts;
-                newAttempts[currentWord] = newWord;
+                newAttempts[currentAttempt] = newWord;
                 return { attempts: newAttempts };
             });
         }
         if (key === 'Enter') {
             this.setState((state) => {
+                const {currentAttempt, attempts} = state;
                 return {
-                    currentWord: state.currentWord + 1
+                    gameFinished: this.answer === attempts[currentAttempt],
+                    currentAttempt: currentAttempt + 1
                 };
-            });
+            },
+            () => this.state.gameFinished && this.onFinish()
+            );
         }
         if (key === 'Backspace') {
             if (currentWordLen > 0){
                 this.setState((state) => {
-                    const {currentWord, attempts} = state;
-                    const newWord = attempts[currentWord].slice(0, currentWordLen-1);
+                    const {currentAttempt, attempts} = state;
+                    const newWord = attempts[currentAttempt].slice(0, currentWordLen-1);
                     const newAttempts = attempts;
-                    newAttempts[currentWord] = newWord;
+                    newAttempts[currentAttempt] = newWord;
                     return { attempts: newAttempts};
                 });
             }
@@ -57,13 +66,13 @@ class Board extends React.Component {
     };
 
     renderBoard = () => {
-        const {attempts, currentWord} = this.state;
+        const {attempts, currentAttempt} = this.state;
         return attempts.map((word, index) =>
             <div key={`row-${index}-${word}`} className='row'>
                 {
                     [...Array(5).keys()].map((char_index) => {
                         const letter = word.charAt(char_index);
-                        let color = currentWord!==index && letter && (letter === this.answer.charAt(char_index) ? 'green' : this.answer.includes(letter) ? 'yellow' :  'gray');
+                        let color = currentAttempt!==index && letter && (letter === this.answer.charAt(char_index) ? 'green' : this.answer.includes(letter) ? 'yellow' :  'gray');
                         const className = 'letterBox ' + color;
                         const key = `${index}-${letter}-${char_index}`;
                         return <LetterBox key={key} letter={letter} className={className}/>;
