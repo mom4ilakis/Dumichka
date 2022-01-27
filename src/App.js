2d4d167678e701e07b6c27ff6c67141228444e08
naydenov <nikolay.naydenov@speedy.io>
2022-01-27 16:05:09 +0200

    added word check algorithm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { checkWord } from './utils/Utils';
 
-const LetterBox = ( props ) => <div className={props.className}> {props.letter} </div>;
+const LetterBox = (props) => <div className={props.className}> {props.letter} </div>;
 
 class Board extends React.Component {
-    constructor( props ) {
-        super( props );
+    constructor(props) {
+        super(props);
         const gameState = this.loadFromLocalStorage();
         const { answer, attempts, currentAttempt, gameFinished } = gameState;
         this.answer = answer || this.generateTodayAnswer();
         this.state = {
-            attempts: attempts || [...Array( 6 ).keys()].map(() => ''),
-            currentAttempt: currentAttempt|| 0,
+            attempts: attempts || [...Array(6).keys()].map(() => ''),
+            currentAttempt: currentAttempt || 0,
             gameFinished: gameFinished || false
         };
     }
 
     componentDidMount = () => {
-        document.addEventListener('keyup', this.handleKeyPress);
+        const { gameFinished } = this.state;
+        if (!gameFinished) {
+            document.addEventListener('keyup', this.handleKeyPress);
+        }
     };
 
     componentWillUnmount = () => {
@@ -30,7 +34,7 @@ class Board extends React.Component {
         document.removeEventListener('keyup', this.handleKeyPress);
     };
 
-    generateTodayAnswer = () => 'weary';
+    generateTodayAnswer = () => 'sissy';
 
     isValidAnswer = () => this.state.attempts[this.state.currentAttempt].length === 5;
 
@@ -62,22 +66,25 @@ class Board extends React.Component {
         window.localStorage.setItem('gameState', JSON.stringify({ 'boardState': JSON.stringify(this.state), 'answer': this.answer }));
     };
 
-    handleKeyPress = ( event ) => {
+    handleKeyPress = (event) => {
         const { key } = event;
         const currentWordLen = this.state.attempts[this.state.currentAttempt].length;
 
-        if ( key.length === 1 && key.match(/[a-z]/i) && currentWordLen < 5 ) {
-            this.setState(( state ) => {
+        if (key.length === 1 && key.match(/[a-z]/i) && currentWordLen < 5) {
+            this.setState((state) => {
                 const { currentAttempt, attempts } = state;
                 const newWord = attempts[currentAttempt] + key;
                 const newAttempts = attempts;
                 newAttempts[currentAttempt] = newWord;
                 return { attempts: newAttempts };
-            } );
+            });
         }
-        if ( key === 'Enter' && this.isValidAnswer()) {
-            this.setState(( state ) => {
+        if (key === 'Enter' && this.isValidAnswer()) {
+            this.setState((state) => {
                 const { currentAttempt, attempts } = state;
+                var todaysWord = this.generateTodayAnswer().toString();
+                var evaluation = checkWord(attempts[currentAttempt], todaysWord);
+                console.log(evaluation);
                 return {
                     gameFinished: this.answer === attempts[currentAttempt] || currentAttempt >= 6,
                     currentAttempt: currentAttempt + 1
@@ -89,31 +96,31 @@ class Board extends React.Component {
             }
             );
         }
-        if ( key === 'Backspace' ) {
-            if ( currentWordLen > 0 ) {
-                this.setState(( state ) => {
+        if (key === 'Backspace') {
+            if (currentWordLen > 0) {
+                this.setState((state) => {
                     const { currentAttempt, attempts } = state;
-                    const newWord = attempts[currentAttempt].slice( 0, currentWordLen-1 );
+                    const newWord = attempts[currentAttempt].slice(0, currentWordLen - 1);
                     const newAttempts = attempts;
                     newAttempts[currentAttempt] = newWord;
                     return { attempts: newAttempts };
-                } );
+                });
             }
         }
     };
 
     renderBoard = () => {
         const { attempts, currentAttempt } = this.state;
-        return attempts.map(( word, index ) =>
+        return attempts.map((word, index) =>
             <div key={`row-${index}-${word}`} className='row'>
                 {
-                    [...Array( 5 ).keys()].map(( char_index ) => {
+                    [...Array(5).keys()].map((char_index) => {
                         const letter = word.charAt(char_index);
-                        let color = currentAttempt!== index && letter && (letter === this.answer.charAt(char_index) ? 'green' : this.answer.includes(letter) ? 'yellow' :  'gray');
+                        let color = currentAttempt !== index && letter && (letter === this.answer.charAt(char_index) ? 'green' : this.answer.includes(letter) ? 'yellow' : 'gray');
                         const className = 'letterBox ' + color;
                         const key = `${index}-${letter}-${char_index}`;
-                        return <LetterBox key={key} letter={letter} className={className}/>;
-                    } )
+                        return <LetterBox key={key} letter={letter} className={className} />;
+                    })
                 }
             </div>
         );
@@ -149,7 +156,7 @@ LetterBox.defaultProps = {
 function App() {
     return (
         <div className="App">
-            <Board/>
+            <Board />
         </div>
     );
 }
